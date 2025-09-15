7892442aa0362ead30271e5ea762619688f92782
hardikgoyal <hardikrgoyal@gmail.com>
2025-09-15 13:03:42 +0530

    Update for production deployment - Mon Sep 15 13:03:42 IST 2025

--- port_operations_app/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ port_operations_app/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import file_picker
 import file_selector_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
-import record_darwin
+import record_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -19,7 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
